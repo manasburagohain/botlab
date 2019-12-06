71f7330ed8abb190f1b279878717e1f09a49a67c
Brian Fogelson <bfogels@umich.edu>
2019-12-06 12:11:16 -0500

    updated pathsafe

--- src/planning/motion_planner.cpp
+++ src/planning/motion_planner.cpp
@@ -54,7 +54,7 @@ bool MotionPlanner::isValidGoal(const pose_xyt_t& goal) const
     float distanceFromPrev = std::sqrt(dx * dx + dy * dy);
 
     //if there's more than 1 frontier, don't go to a target that is within a robot diameter of the current pose
-    if(num_frontiers != 1 && distanceFromPrev < 2 * searchParams_.minDistanceToObstacle) return false;
+    //if(num_frontiers != 1 && distanceFromPrev < 2 * searchParams_.minDistanceToObstacle) return false;
 
     auto goalCell = global_position_to_grid_cell(Point<double>(goal.x, goal.y), distances_);
 
