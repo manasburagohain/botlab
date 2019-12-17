d952719f4498a890db93cd8ba9db813224536553
ndivekar <ndivekar@umich.edu>
2019-12-17 11:46:13 -0500

    updated frontiers

--- src/planning/motion_planner.cpp
+++ src/planning/motion_planner.cpp
@@ -123,6 +123,6 @@ void MotionPlanner::setMap(const OccupancyGrid& map)
 void MotionPlanner::setParams(const MotionPlannerParams& params)
 {
     searchParams_.minDistanceToObstacle = params_.robotRadius;
-    searchParams_.maxDistanceWithCost = 15.0 * searchParams_.minDistanceToObstacle; // was 10, 8 seemed to make bigger
+    searchParams_.maxDistanceWithCost = 20.0 * searchParams_.minDistanceToObstacle; // was 10, 8 seemed to make bigger
     searchParams_.distanceCostExponent = 1.0;
 }
