d9103f1ac755185926f0232fde48b361b476ab7f
ndivekar <ndivekar@umich.edu>
2019-12-16 23:22:19 -0500

    e

--- src/planning/frontiers.cpp
+++ src/planning/frontiers.cpp
@@ -142,7 +142,7 @@ robot_path_t plan_path_to_frontier(const std::vector<frontier_t>& frontiers,
     // Search around the closest frontier until you find the closest point that you can get to
     bool foundPose = false;
     float square_radius = .05;
-    float sq_len = .1; // .05;
+    float sq_len = .05; // .05;
     pose_xyt_t goal_pose;
     while (!foundPose) {
         std::cout << "Looking for free spot to find path to!\n";
